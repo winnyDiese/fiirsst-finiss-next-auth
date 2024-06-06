f60c725c24a5a12c4f5a023ba162db7db487b737
winnyDiese <winnydiese@gmail.com>
2024-06-06 14:29:13 +0200

    bybyb

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -6,20 +6,23 @@ import User from "../../../../models/userModel";
 export async function POST(req){
     try {
         await connectDb()
+        
         const {username, email, password} = await req.json()
         console.log({username, email, password})
         const exists = await User.findOne({$or:[{email},{username}]})
 
         if(exists){
-            return NextResponse.json({message:"User registered with succefully !"})
+            return NextResponse.json({message:"Username or email already exist !"})
             {status:500}
         }
-
+        
         const hashedPassword = await bcrypt.hash(password,10)
         await User.create({username,email,password:hashedPassword})
-        return
+        return NextResponse.json({message:"User registered  !"},{status:201})
 
     } catch (error) {
         console.log("Error while you register user.",error)
+        return NextResponse.json({message:"Error occured while registering the user !"},{status:500})
+
     }
 }
